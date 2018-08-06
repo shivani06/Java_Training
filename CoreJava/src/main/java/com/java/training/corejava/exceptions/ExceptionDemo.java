c50b8e3699b4edd928a6026a76bec61b3ae12e36
shivani06 <shivani6595@gmail.com>
2018-08-06 18:14:53 -0500

    changes made

--- CoreJava/src/main/java/com/java/training/corejava/exceptions/ExceptionDemo.java
+++ CoreJava/src/main/java/com/java/training/corejava/exceptions/ExceptionDemo.java
@@ -1,5 +1,26 @@
 package com.java.training.corejava.exceptions;
 
-public class ExceptionDemo {
-
+//public class ExceptionDemo {
+class customexception extends Exception 
+{
+		
+		public customexception(String s) 
+		{
+			super(s);
+		}
+		public static void main(String[] args)
+		{
+		try 
+		{
+			throw new customexception("hello");
+		}
+		catch(customexception ex) 
+		{
+			System.out.println("exception handled");
+			System.out.println(ex.getMessage());
+		}
+		
+	}
 }
+
+
