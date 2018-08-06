6e2ac62a087d880ec570cc386c1d948a33adbe26
shivani06 <shivani6595@gmail.com>
2018-08-06 18:04:02 -0500

    Changes

--- CoreJava/src/main/java/com/java/training/corejava/Collection/ArrayListDemo.java
+++ CoreJava/src/main/java/com/java/training/corejava/Collection/ArrayListDemo.java
@@ -1,5 +1,11 @@
 package com.java.training.corejava.Collection;
 
 public class ArrayListDemo {
+	
+	public static void main(String[] a) {
+		System.out.println("My First Program");
+		
+		
+	}
 
 }
