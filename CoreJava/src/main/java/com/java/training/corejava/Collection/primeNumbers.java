f4aa8c2fc7772df6849657d44505ce3d11d2c730
shivani06 <shivani6595@gmail.com>
2018-08-07 12:19:01 -0500

    new commit

--- CoreJava/src/main/java/com/java/training/corejava/Collection/primeNumbers.java
+++ CoreJava/src/main/java/com/java/training/corejava/Collection/primeNumbers.java
@@ -3,31 +3,33 @@ import java.util.Scanner;
 
 public class primeNumbers {
 
-		static void primes()
+	public static void main(String[] args) {
 		{
 			int i;
 			//int m;
 			Scanner m = new Scanner(System.in);
 			System.out.println("enter an integer:");
 			int n1 = m.nextInt();
-			for (i=2;i<n1-1;i++)
+			for (i=2;i<=n1-1;i++)
 			if (n1%2!=0 && n1%i!=0)
-				System.out.println("Is a prime number");
+				System.out.println(n1+"Is a prime number");
 			else
 				System.out.println("Is not a prime number");
 				
 				//break;
 				//return true;
 		}
+
+	}
+}
+
 		
 
-		public static void main(String[] args) {
+		
 			
 			//examples obj1 = new examples();
-			primes();		
-		}
-	}
-
+			//primes();		
+	
 
 
 
